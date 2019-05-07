89cd29bac23af7fcb4a8d3f3ef5d15f194e17f02
Timo Czotscher <timocz@t-online.de>
2019-05-07 21:45:45 +0200

    edited navigation drawer

--- app/src/main/java/com/othregensburg/ourglass/MainActivity.java
+++ app/src/main/java/com/othregensburg/ourglass/MainActivity.java
@@ -1,5 +1,6 @@
 package com.othregensburg.ourglass;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -74,23 +75,16 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
+        if (id == R.id.nav_homescreen) {
+            /*
+            Intent startHomescreen = new Intent(this, MainActivity.class);
+            startActivity(startHomescreen);
+            */
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
