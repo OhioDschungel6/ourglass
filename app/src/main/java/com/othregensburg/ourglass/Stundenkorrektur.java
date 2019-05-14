01a351d1435e251f3cf2e14daa94c5486d3c1eee
Your Name <you@example.com>
2019-05-14 09:55:12 +0200

    Test add LinearLayout

--- app/src/main/java/com/othregensburg/ourglass/Stundenkorrektur.java
+++ app/src/main/java/com/othregensburg/ourglass/Stundenkorrektur.java
@@ -18,30 +18,55 @@ import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.sql.Date;
+import java.sql.Time;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 
 public class Stundenkorrektur extends AppDrawerBase {
 
-    int mYear,mMonth,mDay;
+    int mYear, mMonth, mDay;
     final Calendar cal = Calendar.getInstance();
+    private List<Time> startDates = new LinkedList<>();
+    private List<Time> endDates = new LinkedList<>();
+
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stundenkorrektur);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton done = findViewById(R.id.save_correct_time);
+        done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+        //ToDo Löschen
+        startDates.add(new Time(9, 0, 0));
+        startDates.add(new Time(10, 0, 0));
+        startDates.add(new Time(12, 0, 0));
+        endDates.add(new Time(9, 25, 0));
+        endDates.add(new Time(10, 25, 0));
+        endDates.add(new Time(13, 10, 0));
+
+
+        if (startDates.size() == endDates.size()) {
+            LinearLayout linearLayout = findViewById(R.id.linLayout);
+
+            linearLayout.addView();
+        }
+
+
+        //DrawerLayout
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -52,14 +77,14 @@ public class Stundenkorrektur extends AppDrawerBase {
         //Datepicker
         TextView date = findViewById(R.id.date);
         mYear = cal.get(Calendar.YEAR);
-        mMonth = cal.get(Calendar.MONTH)+1;
+        mMonth = cal.get(Calendar.MONTH) + 1;
         mDay = cal.get(Calendar.DAY_OF_MONTH);
-        date.setText(String.format(Locale.GERMAN,"%d.%d.%d",mDay,mMonth,mYear));
+        date.setText(String.format(Locale.GERMAN, "%d.%d.%d", mDay, mMonth, mYear));
         LinearLayout datepickerbar = findViewById(R.id.datepicker_bar);
         datepickerbar.setOnClickListener(e -> {
-                    DatePickerDialog dpd = new DatePickerDialog(this, dl, mYear, mMonth-1, mDay);
-                    dpd.show();
-                });
+            DatePickerDialog dpd = new DatePickerDialog(this, dl, mYear, mMonth - 1, mDay);
+            dpd.show();
+        });
 
 
     }
@@ -68,10 +93,10 @@ public class Stundenkorrektur extends AppDrawerBase {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
             mYear = year;
-            mMonth = month+1;
+            mMonth = month + 1;
             mDay = dayOfMonth;
             TextView date = findViewById(R.id.date);
-            date.setText(String.format(Locale.GERMAN,"%d.%d.%d",mDay,mMonth,mYear));
+            date.setText(String.format(Locale.GERMAN, "%d.%d.%d", mDay, mMonth, mYear));
         }
     };
 
