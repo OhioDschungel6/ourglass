b7a9bdccf7cc38fb44636c323413e6fd753c95fe
andi <andreas.stautner@st.oth-regensburg.de>
2019-05-19 16:14:04 +0200

    chaned everything to recycler View, updated Listener

--- app/src/main/java/com/othregensburg/ourglass/Stundenkorrektur.java
+++ app/src/main/java/com/othregensburg/ourglass/Stundenkorrektur.java
@@ -4,30 +4,23 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.support.v4.view.GravityCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.widget.DatePicker;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-import java.sql.Date;
 import java.sql.Time;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 
@@ -35,10 +28,8 @@ public class Stundenkorrektur extends AppDrawerBase {
 
     int mYear, mMonth, mDay;
     final Calendar cal = Calendar.getInstance();
-    private List<Time> startDates = new LinkedList<>();
-    private List<Time> endDates = new LinkedList<>();
-    int changeIndex;
-    boolean startDateChange;
+    private List<Pair<Time,Time>> dates = new ArrayList<>();
+    Time changedTime;
     TextView changedView;
 
 
@@ -49,23 +40,28 @@ public class Stundenkorrektur extends AppDrawerBase {
         setContentView(R.layout.activity_stundenkorrektur);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton done = findViewById(R.id.save_correct_time);
-        done.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         //ToDo Löschen
-        startDates.add(new Time(9, 0, 0));
-        startDates.add(new Time(10, 0, 0));
-        startDates.add(new Time(12, 0, 0));
-        endDates.add(new Time(9, 25, 0));
-        endDates.add(new Time(10, 25, 0));
-        endDates.add(new Time(13, 10, 0));
+        dates.add(new Pair<>(new Time(9, 0, 0),new Time(9, 25, 0)));
+        dates.add(new Pair<>(new Time(12, 0, 0),new Time(12, 25, 0)));
+        dates.add(new Pair<>(new Time(13, 10, 0),new Time(13, 25, 0)));
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        StundenkorrekturAdapter mAdapter = new StundenkorrekturAdapter(this, dates);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
+        //Plus Button
+        FloatingActionButton addTime = findViewById(R.id.addTime);
+        addTime.setOnClickListener(e->{
+            Time second=dates.get(dates.size()-1).second;
+            dates.add(new Pair<>(new Time(second.getTime()), new Time(second.getTime())));
+            mAdapter.notifyDataSetChanged();
+        });
+
+
+        /*
         if (startDates.size() == endDates.size()) {
             LinearLayout linearLayout = findViewById(R.id.linLayout);
             LayoutInflater inflater= getLayoutInflater();
@@ -74,7 +70,7 @@ public class Stundenkorrektur extends AppDrawerBase {
                 LinearLayout element = (LinearLayout) inflater.inflate(R.layout.entry, linearLayout);
                 TextView s = element.getChildAt(i).findViewById(R.id.startTime);
                 TextView e = element.getChildAt(i).findViewById(R.id.endTime);
-                //element.getChildAt(i).setTag(i);
+
                 s.setText(String.format(Locale.GERMAN,"%2d.%02d", startDates.get(i).getHours(), startDates.get(i).getMinutes()));
                 e.setText(String.format(Locale.GERMAN,"%2d.%02d", endDates.get(i).getHours(), endDates.get(i).getMinutes()));
                 s.setOnClickListener(h->{
@@ -82,7 +78,7 @@ public class Stundenkorrektur extends AppDrawerBase {
                     LinearLayout parent = (LinearLayout)r.getParent();
                     changeIndex = parent.indexOfChild(r);
                     startDateChange=true;
-                    TimePickerDialog tpd = new TimePickerDialog(this, onTimeDialogCallback, 0, 0, true);
+                    TimePickerDialog tpd = new TimePickerDialog(this, onTimeDialogCallback, startDates.get(changeIndex).getHours(), startDates.get(changeIndex).getMinutes(), true);
                     tpd.show();
                 });
                 e.setOnClickListener(h->{
@@ -90,7 +86,7 @@ public class Stundenkorrektur extends AppDrawerBase {
                     LinearLayout parent = (LinearLayout)r.getParent();
                     changeIndex = parent.indexOfChild(r);
                     startDateChange=false;
-                    TimePickerDialog tpd = new TimePickerDialog(this, onTimeDialogCallback, 0, 0, true);
+                    TimePickerDialog tpd = new TimePickerDialog(this, onTimeDialogCallback, endDates.get(changeIndex).getHours(), endDates.get(changeIndex).getMinutes(), true);
                     tpd.show();
                 });
                 FloatingActionButton remove = element.getChildAt(i).findViewById(R.id.remove);
@@ -118,6 +114,7 @@ public class Stundenkorrektur extends AppDrawerBase {
                 startDates.add(new Time(8,0,0));
                 endDates.add(new Time(8,0,0));
             }
+
 
 
             LinearLayout linearLayout = findViewById(R.id.linLayout);
@@ -164,7 +161,7 @@ public class Stundenkorrektur extends AppDrawerBase {
 
 
 
-
+*/
 
         //DrawerLayout
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -174,6 +171,8 @@ public class Stundenkorrektur extends AppDrawerBase {
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+
         //Datepicker
         TextView date = findViewById(R.id.date);
         mYear = cal.get(Calendar.YEAR);
@@ -188,7 +187,7 @@ public class Stundenkorrektur extends AppDrawerBase {
 
 
     }
-
+/*
     private TimePickerDialog.OnTimeSetListener onTimeDialogCallback= new TimePickerDialog.OnTimeSetListener() {
         @Override
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -202,7 +201,7 @@ public class Stundenkorrektur extends AppDrawerBase {
             changedView.setText(String.format(Locale.GERMAN,"%2d.%02d", hourOfDay, minute));
         }
     };
-
+*/
     private DatePickerDialog.OnDateSetListener dl = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
---
+++ app/src/main/java/com/othregensburg/ourglass/StundenkorrekturAdapter.java
@@ -0,0 +1,99 @@
+package com.othregensburg.ourglass;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+
+public class StundenkorrekturAdapter extends RecyclerView.Adapter<StundenkorrekturAdapter.ViewHolder> {
+    private LayoutInflater mInflater;
+    private List<Pair<Time, Time>> dates;
+    private final Context context;
+    Time changedTime;
+
+
+    StundenkorrekturAdapter(Context context, List<Pair<Time,Time>> dates) {
+        mInflater = LayoutInflater.from(context);
+        this.dates = dates;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
+        View mItemView = mInflater.inflate(R.layout.entry, parent, false);
+        return new ViewHolder(mItemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int position) {
+        viewHolder.startTime.setText(String.format(Locale.GERMAN, "%2d.%02d", dates.get(position).first.getHours(), dates.get(position).first.getMinutes()));
+        viewHolder.endTime.setText(String.format(Locale.GERMAN, "%2d.%02d", dates.get(position).second.getHours(), dates.get(position).second.getMinutes()));
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return dates.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView startTime;
+        final TextView endTime;
+        final FloatingActionButton removeButton;
+        ViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            startTime=itemView.findViewById(R.id.startTime);
+            endTime=itemView.findViewById(R.id.endTime);
+            removeButton = itemView.findViewById(R.id.remove);
+            startTime.setOnClickListener(h->{
+                LinearLayout r=(LinearLayout)h.getParent();
+                RecyclerView parent = (RecyclerView)r.getParent();
+                int nr = parent.indexOfChild(r);
+                changedTime= dates.get(nr).first;
+                TimePickerDialog tpd = new TimePickerDialog(context, onTimeDialogCallback, dates.get(nr).first.getHours(), dates.get(nr).second.getMinutes(), true);
+                tpd.show();
+            });
+            endTime.setOnClickListener(h->{
+                LinearLayout r=(LinearLayout)h.getParent();
+                RecyclerView parent = (RecyclerView)r.getParent();
+                int nr = parent.indexOfChild(r);
+                changedTime= dates.get(nr).second;
+                TimePickerDialog tpd = new TimePickerDialog(context, onTimeDialogCallback, dates.get(nr).first.getHours(), dates.get(nr).second.getMinutes(), true);
+                tpd.show();
+            });
+            removeButton.setOnClickListener(h->{
+                LinearLayout r=(LinearLayout)h.getParent();
+                RecyclerView parent = (RecyclerView)r.getParent();
+                int nr = parent.indexOfChild(r);
+                dates.remove(nr);
+                notifyDataSetChanged();
+            });
+        }
+        private TimePickerDialog.OnTimeSetListener onTimeDialogCallback= new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                changedTime.setHours(hourOfDay);
+                changedTime.setMinutes(minute);
+                notifyDataSetChanged();
+            }
+        };
+    }
+}
