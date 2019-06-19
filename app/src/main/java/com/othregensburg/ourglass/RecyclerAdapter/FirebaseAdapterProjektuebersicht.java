12990a29f79e1d530bdcbb6fbbe9b6d161c24ed9
andi <andreas.stautner@st.oth-regensburg.de>
2019-06-19 14:53:09 +0200

    pieChart in Projektübersicht

--- app/src/main/java/com/othregensburg/ourglass/RecyclerAdapter/FirebaseAdapterProjektuebersicht.java
+++ app/src/main/java/com/othregensburg/ourglass/RecyclerAdapter/FirebaseAdapterProjektuebersicht.java
@@ -1,7 +1,12 @@
 package com.othregensburg.ourglass.RecyclerAdapter;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +15,27 @@ import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.othregensburg.ourglass.R;
 import com.othregensburg.ourglass.entity.Projektmitglied;
 import com.othregensburg.ourglass.entity.Stamp;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
 
 public class FirebaseAdapterProjektuebersicht extends FirebaseRecyclerAdapter<Projektmitglied, FirebaseAdapterProjektuebersicht.ViewHolder> {
 
+
+    private int nextColor=0;
     public FirebaseAdapterProjektuebersicht(@NonNull FirebaseRecyclerOptions<Projektmitglied> options) {
         super(options);
     }
@@ -42,12 +59,65 @@ public class FirebaseAdapterProjektuebersicht extends FirebaseRecyclerAdapter<Pr
     class ViewHolder extends RecyclerView.ViewHolder {
         final TextView name;
         final TextView zeit;
+        private static final int PIE_CHART_TEXTSIZE = 14;
 
         ViewHolder(@NonNull final View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.name_field);
             zeit = itemView.findViewById(R.id.hour_field);
 
+            itemView.setOnClickListener(v -> {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(itemView.getContext());
+                PieChartView pie = new PieChartView(itemView.getContext());
+                List<SliceValue> sliceValuesList = new ArrayList<>();
+
+                dialog.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                getRef(getAdapterPosition()).child("taetigkeiten").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        PieChartData pieChartData = new PieChartData(sliceValuesList);
+                        pieChartData.setHasLabels(true).setValueLabelTextSize(PIE_CHART_TEXTSIZE);
+                        for (DataSnapshot child : dataSnapshot.getChildren()) {
+                            //todo nicht Projektmitglied missbrauchen
+                            Projektmitglied pm = child.getValue(Projektmitglied.class);
+                            sliceValuesList.add(new SliceValue(pm.zeit).setLabel(pm.name).setColor(getNextColor()));
+                        }
+                        pie.setPieChartData(pieChartData);
+                        dialog.setView(pie);
+                        dialog.show();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
+
+            });
+
         }
+    }
+    private int getNextColor () {
+        int color = Color.RED;;
+        switch (nextColor%4) {
+            case 0: color = Color.RED;
+                break;
+            case 1: color = Color.YELLOW;
+                break;
+            case 2: color = Color.GREEN;
+                break;
+            case 3: color = Color.BLUE;
+        }
+        nextColor++;
+        return color;
     }
 }
