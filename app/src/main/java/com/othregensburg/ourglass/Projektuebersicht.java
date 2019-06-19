5091e0e3a1c88ab82d4efc3a455a96d2dee5425a
andi <andreas.stautner@st.oth-regensburg.de>
2019-06-19 14:13:03 +0200

    hotfix

--- app/src/main/java/com/othregensburg/ourglass/Projektuebersicht.java
+++ app/src/main/java/com/othregensburg/ourglass/Projektuebersicht.java
@@ -43,14 +43,7 @@ public class Projektuebersicht extends AppDrawerBase {
         setContentView(R.layout.activity_projektuebersicht);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setCheckedItem(R.id.nav_projekt);
